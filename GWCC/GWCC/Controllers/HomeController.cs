930c4255b543007a072ea30876e910b7dc205130
Jack-HA11 <jack@HA11.com.au>
2018-10-16 21:49:39 +1030

    JH-Getting ready for release

--- GWCC/GWCC/Controllers/HomeController.cs
+++ GWCC/GWCC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace GWCC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About US";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us";
 
             return View();
         }
---
+++ GWCC/GWCC/Controllers/ParentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GWCC.Controllers
+{
+    public class ParentsController : Controller
+    {
+        // GET: Parents
+        public ActionResult NeedToKnow()
+        {
+            return View();
+        }
+    }
+}
